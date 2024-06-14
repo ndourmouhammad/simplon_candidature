bdbd012ebc925482c0c183784e7ba52d5d705a16
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-06-14 11:00:14 +0000

    ici je me suis chargé du style de la partie de modification du statut de la candidature

--- resources/views/candidatures/detail.blade.php
+++ resources/views/candidatures/detail.blade.php
@@ -1,56 +1,241 @@
 <!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Détails de la Candidature</title>
-    <!-- Inclusion de Bootstrap CSS pour le style -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Plateforme de gestion des candidatures de Simplon SENEGAL</title>
+    <link rel="stylesheet" href="{{ asset('candidatss/style.css') }}" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,400;6..12,700&display=swap');
+body {
+  font-family: 'Nunito Sans', sans-serif;
+}
+
+.sidebar .nav-link.active {
+  color: #504DC1;
+  background-color: #F1F5F8;
+}
+
+.sidebar .nav-link {
+  color: #333;
+}
+
+.sidebar .nav-link:hover {
+  color: #F1F5F8;
+  background-color: #504DC1;
+}
+
+.header1 {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+
+.table-responsive {
+  margin-top: 20px;
+}
+
+.filtre {
+  margin-top: 20px;
+}
+
+.filtre .input-group {
+  width: 100%;
+}
+
+.logo {
+  width: 150px;
+  margin: 20px auto;
+  display: block;
+}
+.table thead th {
+  background-color: #c3002f;
+  color: white;
+  
+}
+
+.titre h5 {
+  color: red;
+}
+.btn-group span {
+  display: inline-block;
+  margin-right: 10px; /* Adjust margin between elements */
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+.header1 h1 {
+  margin: 0;
+}
+.action {
+  display: flex;
+  justify-content: space-around;
+}
+
+nav {
+  height: 1024px;
+}
+
+/* Détail candidat */
+
+.profile-card {
+  border: 1px solid #e0e0e0;
+  border-radius: 10px;
+  padding: 20px;
+  max-width: 300px;
+  margin: 20px auto;
+  text-align: center;
+}
+.profile-card .profile-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-bottom: 15px;
+}
+.profile-card .profile-info {
+  margin-bottom: 15px;
+}
+.profile-card .profile-info .formation {
+  color: #555;
+}
+.profile-card .contact-icons i {
+  font-size: 20px;
+  color: #e74c3c;
+  margin: 10px;
+}
+.profile-card .pdf-link {
+  margin-top: 20px;
+}
+
+.contact-icons a {
+  display: inline-block;
+  margin-right: 25px; /* Adjust margin between contact icons */
+  margin-bottom: 10px; /* Add bottom margin for better spacing */
+  text-decoration: none;
+  color: #333;
+
+}
+.profile-info h4, .biographie h4, .description h4 {
+  margin-bottom: 15px; /* Add bottom margin for section titles */
+}
+.profile-info, .biographie, .description, .pdf-link {
+  margin-bottom: 30px; /* Add bottom margin between sections */
+}
+
+
+/* .nom {
+  display: flex;
+  gap: 2rem;
+} */
+
+.btn-group {
+  display: flex;
+  flex-direction: column
+}
+    </style>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <img class="logo" src="{{ asset('img/logo.png') }}" alt="Simplon Logo" />
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a href="#" class="nav-link ">
+                    <img src="{{ asset('img/dashboard-24.svg') }}" alt="tableau"> 
+                    <span>Tableau de bord</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link ">
+                  <img src="{{ asset('img/black.svg') }}" alt="formation"> 
+                  <span>Formations</span>
+              </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link active">
+                    <img src="{{ asset('img/person-24.svg') }}" alt="candidats"> 
+                    <span>Candidats</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <a href="#" class="nav-link">
+                    <img src="{{ asset('img/candidats.svg') }}" alt="candidature"> 
+                    <span>Candidatures</span>
+                </a>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <img src="{{ asset('img/logout-24dp-fill0-wght400-grad0-opsz24-1.svg') }}" alt="deconnexion"> 
+                    <span>Déconnexion</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              
+              <div class="col-md-8">
+                  <div class="titre">
+                      <h5>Plateforme de gestion des candidatures de Simplon SENEGAL</h5>
+                  </div>
+              </div>
+              <div class="col-md-4 text-md-right">
+                <div class="btn-group">
+                  
+                  {{-- <span class="font-weight-bold">{{ Auth::user()->prenom }} {{ Auth::user()->nom }}</span> --}}
+                  {{-- <span>{{ Auth::user()->role }}</span> --}}
+              </div>
+              </div>
+  
+            </div>
+
     <!-- Conteneur principal -->
     <div class="container mt-5">
-        <h1>Détails de la Candidature</h1>
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">Biographie</h5>
-                <p class="card-text">{{ $candidature->biographie }}</p>
-            </div>
-        </div>
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">Motivation</h5>
-                <p class="card-text">{{ $candidature->motivation }}</p>
-            </div>
-        </div>
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">Informations de l'Utilisateur</h5>
-                <p><strong>Nom :</strong> {{ $candidature->user->nom }}</p>
-                <p><strong>Prénom :</strong> {{ $candidature->user->prenom }}</p>
-                <p><strong>Email :</strong> {{ $candidature->user->email }}</p>
-                <p><strong>Téléphone :</strong> {{ $candidature->user->telephone }}</p>
-                <p><strong>Date de naissance :</strong> {{ $candidature->user->date_naissance }}</p>
-            </div>
-        </div>
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">CV Professionnel</h5>
-                @if ($candidature->cv_professionnel)
-                    <div class="embed-responsive embed-responsive-4by3">
-                        <iframe class="embed-responsive-item" src="{{ asset('storage/' . $candidature->cv_professionnel) }}" style="width: 100%; height: 500px;"></iframe>
-                    </div>
-                @else
-                    <p>Aucun CV téléchargé</p>
-                @endif
-            </div>
-        </div>
-        <a href="/candidature" class="btn btn-primary">Retour</a>
-        <a href="{{ route('candidature.edit', ['id' => $candidature->id]) }}" class="btn btn-warning mt-3">Modifier</a>
+        <h1>Validation de la candidature</h1>
+       
+        <div class="profile-info">
+            <h4 class="candidate-name">{{ $candidature->user->prenom }} {{ $candidature->user->nom }}</h4>
+            <p class="formation">Formation postuleé : Adefnipa</p>
+            <p class="card-text">{{ $cohorte->libelle }}</p>
 
+        </div>
+        <div class="contact-icons mb-4">
+            <a href="#"><img src="{{ asset('img/icon.svg') }}" alt="" class="mr-2 ">{{ $candidature->user->date_naissance }}</a>
+            <a href="#"><img src="{{ asset('img/Phone.svg') }}" alt="" class="mr-2">{{ $candidature->user->telephone }}</a>
+            <a href="#"><img src="{{ asset('img/Mail.svg') }}" alt="" class="mr-2">{{ $candidature->user->email }}</a>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Biographie</h5>
+            <p class="card-text">{{ $candidature->biographie }}</p>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Motivation</h5>
+            <p class="card-text">{{ $candidature->motivation }}</p>
+        </div>
+        <div class="pdf-link">
+            <a href="path/to/cv.pdf" target="_blank"><img src="{{ asset('img/image5.png') }}" alt=""></a>
+        </div>
     </div>
 
-    <!-- Inclusion de Bootstrap JS pour les fonctionnalités interactives -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</main>
+</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+            
--- resources/views/candidatures/edit.blade.php
+++ resources/views/candidatures/edit.blade.php
@@ -1,66 +1,285 @@
+<!doctype html>
 <!DOCTYPE html>
-<html>
-<head>
+<html lang="fr">
+  <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KY4o8EID6gzl32nTbDZnO4QEOQn19TkvT0eO6h5bZMtLqFJumHsygHg2A+3LBmR1EnbEJ6fnXw9HBEOU8t19RQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="{{ asset('formationCss/accueil.css') }}" />
+    <title>Plateforme de gestion des candidatures de Simplon SENEGAL</title>
+    <link rel="stylesheet" href="{{ asset('candidatss/style.css') }}" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
     <style>
-        body {
-            background-color: #F0F0F0;
-            font-family: 'nunito-sans', sans-serif;
+        @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,400;6..12,700&display=swap');
+body {
+  font-family: 'Nunito Sans', sans-serif;
+}
+
+.sidebar .nav-link.active {
+  color: #504DC1;
+  background-color: #F1F5F8;
+}
+
+.sidebar .nav-link {
+  color: #333;
+}
+
+.sidebar .nav-link:hover {
+  color: #F1F5F8;
+  background-color: #504DC1;
+}
+
+.header1 {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+
+.table-responsive {
+  margin-top: 20px;
+}
+
+.filtre {
+  margin-top: 20px;
+}
+
+.filtre .input-group {
+  width: 100%;
+}
+
+.logo {
+  width: 150px;
+  margin: 20px auto;
+  display: block;
+}
+.table thead th {
+  background-color: #c3002f;
+  color: white;
+  
+}
+
+.titre h5 {
+  color: red;
+}
+.btn-group span {
+  display: inline-block;
+  margin-right: 10px; /* Adjust margin between elements */
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+.header1 h1 {
+  margin: 0;
+}
+.action {
+  display: flex;
+  justify-content: space-around;
+}
+
+nav {
+  height: 1024px;
+}
+
+/* Détail candidat */
+
+.profile-card {
+  border: 1px solid #e0e0e0;
+  border-radius: 10px;
+  padding: 20px;
+  max-width: 300px;
+  margin: 20px auto;
+  text-align: center;
+}
+.profile-card .profile-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-bottom: 15px;
+}
+.profile-card .profile-info {
+  margin-bottom: 15px;
+}
+.profile-card .profile-info .formation {
+  color: #555;
+}
+.profile-card .contact-icons i {
+  font-size: 20px;
+  color: #e74c3c;
+  margin: 10px;
+}
+.profile-card .pdf-link {
+  margin-top: 20px;
+}
+
+.contact-icons a {
+  display: inline-block;
+  margin-right: 25px; /* Adjust margin between contact icons */
+  margin-bottom: 10px; /* Add bottom margin for better spacing */
+  text-decoration: none;
+  color: #333;
+
+}
+.profile-info h4, .biographie h4, .description h4 {
+  margin-bottom: 15px; /* Add bottom margin for section titles */
+}
+.profile-info, .biographie, .description, .pdf-link {
+  margin-bottom: 30px; /* Add bottom margin between sections */
+}
+
+/* .nom {
+  display: flex;
+  gap: 2rem;
+} */
+
+.btn-group {
+  display: flex;
+  flex-direction: column
+}
+  /* Style personnalisé pour le formulaire de modification du statut */
+  .form-group select {
+            width: 195px;
+            height: 36px;
+            border-radius: 8px;
         }
-        .navbar-light {
-            background-color: #fff !important;
+        .btn-custom {
+            background-color: red;
+            border-color: red;
+            width: 195px;
+            height: 36px;
+            border-radius: 8px;
+        }
+        .btn-custom:hover {
+            background-color: darkred;
+            border-color: darkred;
+        }
+        .form-container {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+        .button-group {
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-end;
+            width: 100%;
+        }
+        .align-right {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+            margin-top: auto;
         }
     </style>
-</head>
-<body>
-    <div class="accueil container-fluid p-0">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#"><img src="https://simplon.sn/wp-content/uploads/2023/07/logo-simplonSenegal.png" alt="Logo" width="130px" class="image" /></a>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">A propos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Nos formations</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="bi bi-person-circle"></i> Mon compte</a>
-                    </li>
-                </ul>
-            </div>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <img class="logo" src="{{ asset('img/logo.png') }}" alt="Simplon Logo" />
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a href="#" class="nav-link ">
+                    <img src="{{ asset('img/dashboard-24.svg') }}" alt="tableau"> 
+                    <span>Tableau de bord</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link ">
+                  <img src="{{ asset('img/black.svg') }}" alt="formation"> 
+                  <span>Formations</span>
+              </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link active">
+                    <img src="{{ asset('img/person-24.svg') }}" alt="candidats"> 
+                    <span>Candidats</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <a href="#" class="nav-link">
+                    <img src="{{ asset('img/candidats.svg') }}" alt="candidature"> 
+                    <span>Candidatures</span>
+                </a>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <img src="{{ asset('img/logout-24dp-fill0-wght400-grad0-opsz24-1.svg') }}" alt="deconnexion"> 
+                    <span>Déconnexion</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </nav>
-
-        <!-- Conteneur principal -->
-        <div class="container mt-5">
-            <h1>Modifier le statut de la candidature</h1>
-            <form action="{{ route('candidature.update', ['id' => $candidature->id]) }}" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="statut">Statut</label>
-                    <select name="statut" id="statut" class="form-control">
-                        <option value="en attente" {{ $candidature->statut == 'en attente' ? 'selected' : '' }}>En attente</option>
-                        <option value="acceptée" {{ $candidature->statut == 'acceptée' ? 'selected' : '' }}>Acceptée</option>
-                        <option value="rejeté" {{ $candidature->statut == 'rejeté' ? 'selected' : '' }}>Rejeté</option>
-                    </select>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              
+              <div class="col-md-8">
+                  <div class="titre">
+                      <h5>Plateforme de gestion des candidatures de Simplon SENEGAL</h5>
+                  </div>
+              </div>
+              <div class="col-md-4 text-md-right">
+                <div class="btn-group">
+                    {{-- <span class="font-weight-bold">{{ Auth::user()->prenom }} {{ Auth::user()->nom }}</span> --}}
+                    {{-- <span>{{ Auth::user()->role }}</span> --}}
                 </div>
-                <button type="submit" class="btn btn-primary">Valider</button>
-            </form>
-        </div>
+            </div>
+  
+            </div>
 
-        <!-- Inclusion de Bootstrap JS pour les fonctionnalités interactives -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- Conteneur principal -->
+    <div class="container mt-5">
+        <h1>Validation de la candidature</h1>
+        <div class="profile-info">
+            <h4 class="candidate-name">{{ $candidature->user->prenom }} {{ $candidature->user->nom }}</h4>
+            <p class="formation">Formation postuleé : Adefnipa</p>
+            <p class="card-text">{{ $candidature->cohorte->libelle }}</p>
+
+        </div>
+        <div class="contact-icons mb-4">
+            <a href="#"><img src="{{ asset('img/icon.svg') }}" alt="" class="mr-2 ">{{ $candidature->user->date_naissance }}</a>
+            <a href="#"><img src="{{ asset('img/Phone.svg') }}" alt="" class="mr-2">{{ $candidature->user->telephone }}</a>
+            <a href="#"><img src="{{ asset('img/Mail.svg') }}" alt="" class="mr-2">{{ $candidature->user->email }}</a>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Biographie</h5>
+            <p class="card-text">{{ $candidature->biographie }}</p>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Motivation</h5>
+            <p class="card-text">{{ $candidature->motivation }}</p>
+        </div>
+        <div class="pdf-link">
+            <a href="path/to/cv.pdf" target="_blank"><img src="{{ asset('img/image5.png') }}" alt=""></a>
+        </div>
+    </div>        <!-- Formulaire de modification du statut -->
+    <div class="align-right mb-3">
+        <h2>Modification du Statut</h2>
+        <form action="{{ route('candidature.update', ['id' => $candidature->id]) }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="statut">Statut</label>
+                <select name="statut" id="statut" class="form-control">
+                    <option value="en attente" {{ $candidature->statut == 'en attente' ? 'selected' : '' }}>En attente</option>
+                    <option value="acceptée" {{ $candidature->statut == 'acceptée' ? 'selected' : '' }}>Acceptée</option>
+                    <option value="rejeté" {{ $candidature->statut == 'rejeté' ? 'selected' : '' }}>Rejeté</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-custom mt-3">Valider</button>
+        </form>
     </div>
+
+   
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
